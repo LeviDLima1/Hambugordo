3c2603e30917e88861bbd065234b694eb629bdba
Levi Duarte <duartelevi1@gmail.com>
2024-06-05 20:31:05 -0300

    melhorando o html e css e começando no javaScript

---
+++ src/js/hamburguers.js
@@ -0,0 +1,5 @@
+let hambuJson = [
+    {id:1, name: 'COMBO CLASSIC', img: 'src/imagens/Combo-Classic.jpg', price:45.00, description: 'Burger (150gr), queijo cheddar fatiado, cebola caramelizada, bacon, maionese temperada no pão australiano + Porção de batata palito com tempero da casa.'},
+    {id:2, name: 'COMBO BACON BBQ', img: 'src/imagens/Combo-Bacon-BBQ.jpg', price:44.00, description: 'Burger (150gr), cheddar, bacon crocante, maionese temperada da casa no pão de brioche + Porção de batata palito com tempero da casa (120g)'}
+    
+]
---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const search_box = document.querySelector('.js-search-box');
+const search_btn = document.querySelector('.open-search');
+const close_btn = document.querySelector('.js-close');
+
+search_btn.addEventListener('click', () => {
+    search_box.classList.add('active');
+});
+
+close_btn.addEventListener('click', () => {
+    search_box.classList.remove('active');
+})
+
+
+hambuJson.map((item, index) => {
+    //console.log(item)
+
+    let hambuItem = document.querySelector('.container-produtos').cloneNode(true)
+    //console.log(hambuItem)
+    
+    document.querySelector('.combos').append(hambuItem)
+    
+    hambuItem.querySelector('.foto-produto img').src = item.img
+    hambuItem.querySelector('.nome-produto').innerHTML = item.name
+    hambuItem.querySelector('.descricao-produto').innerHTML = item.description
+    hambuItem.querySelector('.preco-produto').innerHTML = `R$ ${item.price}`
+
+
+    hambuItem.querySelector('.produto').addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log('Clicou na pizza')
+    })
+})
+
+
+
+        
