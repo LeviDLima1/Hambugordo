3b4e552e103ca569da24eab68a6900b83eae126d
Levi Duarte <duartelevi1@gmail.com>
2024-06-10 14:26:31 -0300

    .

--- src/js/hamburguers.js
+++ src/js/hamburguers.js
@@ -3,27 +3,3 @@ let hambuJson = [
     {id:2, name: 'COMBO BACON BBQ', img: 'src/imagens/Combo-Bacon-BBQ.jpg', price:44.00, description: 'Burger (150gr), cheddar, bacon crocante, maionese temperada da casa no pão de brioche + Porção de batata palito com tempero da casa (120g)'}
     
 ]
-
-let addJson = [
-    {id: 1, name: 'CHEDDAR FATIADO', price: 3},
-    {id: 2, name: 'FATIA BACON', price: 6},
-    {id: 3, name: 'CARNE EXTRA 150G', price: 8},
-    {id: 4, name: 'CREAM CHEESE', price: 4},
-    {id: 5, name: 'CARNE DO SOL', price: 5},
-    {id: 6, name: 'CALABRESA FATIADA', price: 3},
-    {id: 7, name: 'BBQ', price: 2}
-]
-
-let bebidaJson = [
-    {id: 1, name: 'COCA COLA LATA'},
-    {id: 2, name: 'COCA ZERO LATA'},
-    {id: 3, name: 'GUARANÁ ANTARCTICA LATA'},
-    {id: 4, name: 'FANTA UVA LATA'},
-    {id: 5, name: 'FANTA LARANJA LATA'},
-]
-
-let carneJson = [
-    {id: 1, name: 'Mal Passado'},
-    {id: 2, name: 'Ao Ponto'},
-    {id: 3, name: 'Bem Passado'}
-]
--- src/js/index.js
+++ src/js/index.js
@@ -22,23 +22,28 @@ hambuJson.map((item, index) => {
     hambuItem.querySelector('.foto-produto img').src = item.img
     hambuItem.querySelector('.nome-produto').innerHTML = item.name
     hambuItem.querySelector('.descricao-produto').innerHTML = item.description
-    hambuItem.querySelector('.preco-produto').innerHTML = `R$ ${item.price}`
+    hambuItem.querySelector('.preco-produto').innerHTML = `R$ ${item.price}`    
 
 
     hambuItem.querySelector('.produto').addEventListener('click', (e) => {
         e.preventDefault()
         console.log('Clicou na pizza')
+
+        document.querySelector('.hambuCombo-Img img').src = item.img
+        document.querySelector('.hambu-Combo-Description').innerHTML = item.description
+        document.querySelector('.hambuCombo-Name').innerHTML = item.name
+        //hambuItem.querySelector('.preco-produto').innerHTML = `R$ ${item.price}`   
+
+
+        document.querySelector('.hambuWindow-Area').style.display = 'block'
+    })
+
+
+    document.querySelector('.hambuWindow-Back').addEventListener('click', () => {
+        document.querySelector('.hambuWindow-Area').style.display = 'none'
     })
 })
 
-addJson.map((item, index) => {
-    console.log(item)
-    let nomeCombo = document.querySelector('.itens-add').cloneNode(true)
-
-    document.querySelector('.container-add').append(nomeCombo)
-    
-    
-})
 
 
 
